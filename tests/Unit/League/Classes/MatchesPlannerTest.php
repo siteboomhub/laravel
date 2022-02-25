6fded2cbed56b3e9bf5cddecf21d27974dfd7e31
myspaceee <vitaliy.sheverov24@gmail.com>
2022-02-25 08:18:49 +0400

    added calculating amount of matches tests

---
+++ tests/Unit/League/Classes/MatchesPlannerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\NewLeague;
+
+use App\Services\League\Classes\Game;
+use App\Services\League\Classes\MatchesPlanner;
+use App\Services\League\Factories\GameFactory;
+use PHPUnit\Framework\TestCase;
+
+class MatchesPlannerTest extends TestCase
+{
+    public function dataProvider()
+    {
+        return [
+            [ [1, 2, 3, 4], 12],
+            [ [1, 2, 3], 6],
+            [ [1, 2, 3, 4, 5], 20]
+        ];
+    }
+
+    /**
+     * @param $teams
+     * @param $expected_count
+     * @throws \App\Exceptions\League\GameMembersException
+     * @dataProvider dataProvider
+     */
+    public function testThatMatchesNumberCalculatedCorrectly($teams, $expected_count)
+    {
+        $game_factory = $this->createStub(GameFactory::class);
+
+        $game_factory->method('build')->willReturn(
+            $this->createStub(Game::class)
+        );
+
+        $planner = new MatchesPlanner(
+            $teams,
+            $game_factory
+        );
+
+        $matches = $planner->plan();
+
+        $this->assertCount($expected_count, $matches);
+
+    }
+}
