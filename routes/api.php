88ee8bf5120c393d94144638a9e68a521e8dbad1
myspaceee <vitaliy.sheverov24@gmail.com>
2022-03-07 03:12:47 +0400

    added routes, views, countroller entries

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(\App\Http\Controllers\LeagueController::class)->prefix('league')->group(function (){
+    Route::post('/', 'create');
+    Route::get('/{leagueUUID}', 'show');
+    Route::post('/{leagueUUID}/play-week', 'playNextWeek');
+    Route::post('/{leagueUUID}/play-all-weeks', 'playAllWeeks');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\League\LeagueFacade;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/league/{leagueUUID}', 'league')->name('league.show');
+
